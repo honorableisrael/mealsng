95c584ff43fe952e826d70f77f09ac68f2b55685
Hilary <israel.hilary7@yahoo.com>
2020-02-20 19:37:52 +0100

    addUpdates

--- javascript/main.js
+++ javascript/main.js
@@ -12,15 +12,17 @@ function addActiveClassToSelectedElement(id){
     }
 }
 
-function addBoxShadowClassToSelectedElement(id){
+function addBoxShadowClassToSelectedElement(id,checkedId){
   var el = document.getElementById(id);
    var classToBeAdded = 'boxshadow'
-     if (!el.classList.contains(classToBeAdded)){
+   var checkbox = document.getElementById(checkedId).checked = document.getElementById(checkedId).checked?false:true
+     if (!el.classList.contains(classToBeAdded) && document.getElementById(checkedId).checked){
          el.classList.add(classToBeAdded)
        return  console.log('true')
      }
-     if (el.classList.contains(classToBeAdded)){   
+     if (el.classList.contains(classToBeAdded) && !document.getElementById(checkedId).checked){   
          el.classList.remove(classToBeAdded)
+        //  var checkbox = document.getElementById(checkedId).checked = false
         return console.log('false')
      }
  }
